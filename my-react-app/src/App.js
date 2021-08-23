0c25808d9db92dfe92f4225a6394392bd22ecaac
trudek <damian.trutkowski@gmail.com>
2021-08-23 23:13:52 +0200

    first React lesson

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
 
 function App() {
+  // const name = 'Damian'
+  // const x = true
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* <h1>hello {name}</h1>
+      <h2>hello {1+1}</h2>
+      <h2>hello {x ? 'yes' : 'no'}</h2> */}
+      <Header />
     </div>
   );
 }
